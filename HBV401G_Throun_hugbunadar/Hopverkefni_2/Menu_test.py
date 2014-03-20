7e1e5e519d708c2e57ef0a16f6f3a1987389865e
hanxer <hanxer@gmail.com>
2014-03-20 13:32:26 +0000

    Update Menu_test.py

--- HBV401G_Throun_hugbunadar/Hopverkefni_2/Menu_test.py
+++ HBV401G_Throun_hugbunadar/Hopverkefni_2/Menu_test.py
@@ -2,6 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import sys
+
+class Highscores(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title, size=(330, 300))
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        panel = wx.Panel(self, -1)
+
+        hs = self.getFile()
+
+        self.list = wx.ListCtrl(panel, -1, style=wx.LC_REPORT)
+        self.list.InsertColumn(0, 'nr.', width=25)
+        self.list.InsertColumn(1, 'name', width=225)
+        self.list.InsertColumn(2, 'time', wx.LIST_FORMAT_RIGHT, 80)
+
+        count = 1
+        for i in hs:
+            index = self.list.InsertStringItem(sys.maxint, str(count))
+            nafn = ""
+            for j in range(len(i)-1):
+                nafn += i[j]+" "
+            self.list.SetStringItem(index, 1, nafn)
+            self.list.SetStringItem(index, 2, i[-1])
+            count += 1
+
+        hbox.Add(self.list, 1, wx.EXPAND)
+        panel.SetSizer(hbox)
+
+        self.Centre()
+        self.Show(True)
+
+    def getFile(self):
+        highscorefile = open("highscores")
+        temp = []
+        hslist = []
+        temp += highscorefile.readlines()
+        highscorefile.close()
+        for i in temp:
+            hslist.append(i.split())
+        hslist = sorted(hslist, key = lambda x: x[-1]) 
+        return hslist
 
 class Example(wx.Frame):
     
@@ -35,15 +77,13 @@ class Example(wx.Frame):
         self.Show(True)
         
     def Start(self, e):
-        #tímabundið
         self.SetTitle('New game')
 
     def OnQuit(self, e):
         self.Close()
 
     def loadHighscore(self, e):
-        #tímabundið-næst inn
-        self.SetTitle('Highscore')
+        Highscores(self, -1, 'Highscores')
 
     def loadAbout(self, e):
     	wx.MessageBox('Tri-Peaks\nÞróun Hugbúnaðar vor 2014\nHópur 30:\nDaníel Sandjárn\nKjartan B. Rough\nDaníel Heimsson\nFunky Oak\nHans Pétursson', 'About', 
