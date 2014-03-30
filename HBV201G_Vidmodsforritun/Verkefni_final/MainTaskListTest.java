26d1d37e482fb07dfcd2f5e1606a54e3bd4eb2f2
Daniel Eldjarn <danieleldjarn@gmail.com>
2014-03-30 23:03:34 +0000

    Updated final project in GUI design

--- HBV201G_Vidmodsforritun/Verkefni_final/MainTaskListTest.java
+++ HBV201G_Vidmodsforritun/Verkefni_final/MainTaskListTest.java
@@ -1,3 +1,6 @@
+// Compile using: javac -cp .:jcalendar_1.4.jar MainTaskListTest.java
+// Run using: java -cp .:jcalendar_1.4.jar MainTaskListTest
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
--- HBV201G_Vidmodsforritun/Verkefni_final/TaskList.java
+++ HBV201G_Vidmodsforritun/Verkefni_final/TaskList.java
@@ -7,8 +7,11 @@ import java.util.Calendar;
 import java.util.Locale;
 import java.text.*;
 import java.io.*;
+import com.toedter.calendar.JCalendar;
 
-public class TaskList extends JFrame {
+import javax.swing.JButton;
+
+public class TaskList extends JFrame implements ActionListener {
 
     private JPanel  panel;
     private JList   list;
@@ -29,13 +32,37 @@ public class TaskList extends JFrame {
         JPanel buttonPanel = new JPanel(new BorderLayout());
         JButton addEventButton = new JButton("Add");
         JButton deleteEventButton = new JButton("Delete");
+        
         addEventButton.setPreferredSize(new Dimension(100, 50));
         deleteEventButton.setPreferredSize(new Dimension(100, 50));
+        
         buttonPanel.add(addEventButton, BorderLayout.WEST);
         buttonPanel.add(deleteEventButton, BorderLayout.EAST);
 
         panel.add(myList, BorderLayout.CENTER);
         panel.add(buttonPanel, BorderLayout.SOUTH);
+        
+        addEventButton.addActionListener(this);
+        addEventButton.setActionCommand("addEvent");
+
+        deleteEventButton.addActionListener(this);
+        deleteEventButton.setActionCommand("deleteEvent");
         setContentPane(panel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String action = ae.getActionCommand();
+        if (action.equals("addEvent")) {
+
+            // Debug code
+            System.out.println("addEventButton pressed");
+            System.out.println("starting calendar");
+
+            JFrame calendar = new Cal();
+            calendar.pack();
+            calendar.setVisible(true);
+            // Call calendar app
+        }
     }
 }
