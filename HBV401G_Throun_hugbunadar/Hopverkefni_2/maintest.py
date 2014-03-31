e569e80e94bebc1084820e79a8f778ee2f796da9
Eiiki <eiiki0808@gmail.com>
2014-03-31 13:43:01 +0000

    Update maintest.py

--- HBV401G_Throun_hugbunadar/Hopverkefni_2/maintest.py
+++ HBV401G_Throun_hugbunadar/Hopverkefni_2/maintest.py
@@ -3,12 +3,10 @@
 
 from Kapall_klasi import *
 
-
 if __name__ == '__main__':
+	tree = Tree()
 	spilastokkur = Stokkur()
 	spilastokkur.nyr_random_stokkur()
-
-	tree = Tree()
 	tree.make_tree(spilastokkur)
 	'''
 	Á þesum tímapunkti er tréið fullt og spilastokkur heldur utan um þau spil sem eftir eru
@@ -18,11 +16,8 @@ if __name__ == '__main__':
 	'''
 	ruslabunki = Stokkur()
 
-	"""
-	Ástæðan fyrir því að þetta keyrir ekki fullkomlega er vegna þess að við gáfum okkur
-	ekki tíma til að uppfæra tréið eftir að fjarlægt er úr því.
-	"""
 	while True:
+		print "\n"
 		if not tree.stokkur.has_next() or not spilastokkur.has_next():
 			break
 		if ruslabunki.spilafjoldi == 0:
@@ -30,14 +25,10 @@ if __name__ == '__main__':
 
 		print "Spilið efst í ruslastokknum er: ",ruslabunki.stokkur[-1]
 		print "Spil sem þú getur fjarlægt úr tréi:"
-		removables = Stokkur()
-		cnt = -1
-		for p in tree.stokkur.stokkur:
-			if tree.is_removeable(p):
-				removables.add(p)
-				cnt += 1
+		removeables = tree.get_removeables()
+		cnt = len(removeables.stokkur)-1
 
-		print removables
+		print removeables
 		what_to_do = raw_input("Veldu 1 til að fjarlægja spil úr tréi, annars verður dregið nýtt spil úr stokki: ")
 		if what_to_do == "1":
 			try:
@@ -45,11 +36,11 @@ if __name__ == '__main__':
 			except:
 				print "Verður að slá inn tölu frá 0 upp í ",cnt
 				continue
-			if tala > cnt:
+			if tala > cnt or tala < 0:
 				print "Verður að slá inn tölu frá 0 upp í ",cnt
 				continue
 
-			delgildi = removables.stokkur[tala].gildi
+			delgildi = removeables.stokkur[tala].gildi
 			if delgildi == 13:
 				legit_remove = [1,12]
 			elif delgildi == 1:
@@ -57,19 +48,23 @@ if __name__ == '__main__':
 			else:
 				legit_remove = [delgildi+1, delgildi-1]
 
+
 			if ruslabunki.stokkur[-1].gildi in legit_remove:
-				ruslabunki.add(removables.get_next(tala))
-				tree.stokkur.remove_pos(tree.stokkur.stokkur.index(ruslabunki.stokkur[-1]))
+				ruslabunki.add(removeables.get_next(tala))
+				tree.update_tree(ruslabunki.stokkur[-1])
 
 			else:
+				print "\n"
+				print "***********************************************"
 				print "Ekki hægt hægt að bæta spili við í ruslabunkann"
+				print "***********************************************"
 
 		else:
 			ruslabunki.add(spilastokkur.get_next())
 
-		print "\n"
-	
 	if tree.stokkur.spilafjoldi == 0:
+		print "stokkur trés: ",tree.stokkur.stokkur
 		print "Þú vannst!"
 	else:
-		print "aaaawwwwwwwww"
+		print "aaaawwwwwwwww, spilin eftir í tréinu eru:"
+		print tree.get_removeables()
