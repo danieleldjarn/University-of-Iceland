38520dbede6f4735c449e4db19bfa97b36855474
Eiiki <eiiki0808@gmail.com>
2014-04-04 16:24:36 +0000

    Update test_kapall.py

--- HBV401G_Throun_hugbunadar/Hopverkefni_2/test_kapall.py
+++ HBV401G_Throun_hugbunadar/Hopverkefni_2/test_kapall.py
@@ -1,23 +1,29 @@
+#!/usr/bin/python
+#coding=utf-8
+
 from Kapall_klasi import Stokkur
 from Kapall_klasi import Tree
 import unittest
 
 class testDeck(unittest.TestCase): 
 
-
     deckCount = 52
     knownCards = ["<H 1>", "<S 5>", "<T 13>", "<L 4>", "<H 10>"]
     bogusCards = ["<B 1>", "<S 14>", "<H K>", "<L 0>"]
 
     def testCardCount(self):
+        'Prófar að fjarlægja öll spil úr stokki'
         deck = Stokkur()
         deck.nyr_random_stokkur()
         tmp = []
+        self.assertTrue(deck.has_next())
         while deck.has_next():
             tmp.append(deck.get_next())
         self.assertEqual(self.deckCount, len(tmp))
+        self.assertFalse(deck.has_next())
 
     def testKnownCards(self):
+        'Athugar hvort að löggild spil séu í fullum stokki'
         deck = Stokkur()
         deck.nyr_random_stokkur()
         tmp = []
@@ -27,6 +33,7 @@ class testDeck(unittest.TestCase):
         self.failUnless(check)
 
     def testBogusCards(self):
+        'Athugar hvort að ólögleg spil séu í fullum stokki'
         deck = Stokkur()
         deck.nyr_random_stokkur()
         tmp = []
@@ -38,41 +45,85 @@ class testDeck(unittest.TestCase):
 
 class testTree(unittest.TestCase):
 
-
     def testTreeWithFullDeck(self):
+        'Setur löggildan spilastokk í tré og athugar hvort ákveðnir hnútar séu ekki eins og þeir eiga að vera'
         deck = Stokkur()
         deck.nyr_random_stokkur()
         tree = Tree()
         tree.make_tree(deck)
-        self.assertNotEqual(tree.root_1, tree.nil)
-        self.assertNotEqual(tree.root_2, tree.nil)
-        self.assertNotEqual(tree.root_3, tree.nil)
-        self.assertNotEqual(tree.root_2.right.right.left, tree.nil)
-        self.assertNotEqual(tree.root_2.left.left.right, tree.nil)
-        self.assertNotEqual(tree.root_3.right.right.right, tree.nil)
+        self.assertIsNot(tree.root_1, tree.nil)
+        self.assertIsNot(tree.root_2, tree.nil)
+        self.assertIsNot(tree.root_3, tree.nil)
+        self.assertIsNot(tree.root_2.right.right.left, tree.nil)
+        self.assertIsNot(tree.root_2.left.left.right, tree.nil)
+        self.assertIsNot(tree.root_3.right.right.right, tree.nil)
+        self.assertIs(tree.root_1.right.right.right, tree.root_2.left.left.left)
+        self.assertIs(tree.root_2.right.right.right, tree.root_3.left.left.left)
+        for i in range(3):
+            if i == 0:
+                root = tree.root_1
+            elif i == 1:
+                root = tree.root_2
+            else:
+                root = tree.root_3
+            self.assertIs(root.left.right, root.right.left)
+            self.assertIs(root.left.left.right, root.right.left.left)
+            self.assertIs(root.left.left.right, root.left.right.left)
+            self.assertIs(root.left.right.right, root.right.right.left)
+            self.assertIs(root.left.right.right, root.right.left.right)
+        self.assertEqual(len(tree.stokkur.stokkur), 28)
 
     def testTreeWithTooFewCards(self):
+        'Athugar hvort það sé hægt að búa til tré með of fáum spilum'
         deck = Stokkur()
         deck.nyr_random_stokkur()
-        for i in xrange(30):
+        for i in range(30):
             tmp = deck.get_next()
         tree = Tree()
         tree.make_tree(deck)
-        self.assertEqual(tree.root_1, tree.nil)
-        self.assertEqual(tree.root_2, tree.nil)
-        self.assertEqual(tree.root_3, tree.nil)
+        self.assertIs(tree.root_1, tree.nil)
+        self.assertIs(tree.root_2, tree.nil)
+        self.assertIs(tree.root_3, tree.nil)
+        self.assertEqual(len(tree.stokkur.stokkur), 0)
 
-    def testNodeDepth(self):
+    def testTreeUpdate(self):
+        'Býr til löggild tré og fjarlægir öll spil úr því og athugar hvort það sé svo örugglega tómt að lokum'
         deck = Stokkur()
         deck.nyr_random_stokkur()
         tree = Tree()
         tree.make_tree(deck)
-        self.assertEqual(tree.node_depth(tree.root_1), 0)
-        self.assertNotEqual(tree.node_depth(tree.root_3),1)
-        self.assertEqual(tree.node_depth(tree.root_2.left), 1)
-        self.assertEqual(tree.node_depth(tree.root_3.left.right), 2)
-        self.assertEqual(tree.node_depth(tree.root_3.left.right.left), 3)
-        self.assertNotEqual(tree.node_depth(tree.root_3.right.left.right), 4)
+        self.assertIsNot(tree.root_1, tree.nil)
+        self.assertIsNot(tree.root_2, tree.nil)
+        self.assertIsNot(tree.root_3, tree.nil)
+        while tree.stokkur.spilafjoldi > 0:
+            tmp = tree.get_removeables()
+            for spil in tmp.stokkur:
+                tree.update_tree(spil) 
+        self.assertIs(tree.root_1, tree.nil)
+        self.assertIs(tree.root_2, tree.nil)
+        self.assertIs(tree.root_3, tree.nil)
+
+    def testPositions(self):
+        'Athugar hvort spil séu ekki með sömu staðsetningu'
+        deck = Stokkur()
+        deck.nyr_random_stokkur()
+        tree = Tree()
+        tree.make_tree(deck)
+        self.assertEqual(tree.card_pos(tree.root_1.right.right.right), tree.card_pos(tree.root_2.left.left.left))
+        self.assertEqual(tree.card_pos(tree.root_2.right.right.right), tree.card_pos(tree.root_3.left.left.left))
+        for i in range(3):
+            if i == 0:
+                root = tree.root_1
+            elif i == 1:
+                root = tree.root_2
+            else:
+                root = tree.root_3
+            self.assertEqual(tree.card_pos(root.left.right), tree.card_pos(root.right.left))
+            self.assertEqual(tree.card_pos(root.left.left.right), tree.card_pos(root.right.left.left))
+            self.assertEqual(tree.card_pos(root.left.left.right), tree.card_pos(root.left.right.left))
+            self.assertEqual(tree.card_pos(root.right.right.left), tree.card_pos(root.left.right.right))
+            self.assertEqual(tree.card_pos(root.right.right.left), tree.card_pos(root.right.left.right))
+
 
 
 if __name__ == '__main__':
