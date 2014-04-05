f4aa38ff0da661650a7c984962835284e420aff0
kjartan <kjartantr1@gmail.com>
2014-04-05 18:50:12 +0000

    prim ready

--- TOL403G_Greining_reiknirita/verkefni_3/inntakUttak/prim.py
+++ TOL403G_Greining_reiknirita/verkefni_3/inntakUttak/prim.py
@@ -1,22 +1,14 @@
 import fileinput
 import sys
+import time
+import copy
 
 
-def main():
-    weight = {}
+def main(noNodes, edges):
     mstPrim = {}
     graph = {}
-    count = 0
 
-    for line in fileinput.input():
-        line = line.split()
-        if not fileinput.isfirstline():
-            weight[(int(line[0]), int(line[1]))] = int(line[2])
-            weight[(int(line[1]), int(line[0]))] = int(line[2])
-        else:
-            count = int(line[0])
-
-    for i in xrange(0, count):
+    for i in xrange(0, noNodes):
         graph[i] = {'key': sys.maxint, 'parent': None}
     graph[0]['key'] = 0
 
@@ -24,11 +16,23 @@ def main():
         lowestKey = getLowestKey(graph) #lowestKey = integer
         vertex = graph.pop(lowestKey) #vertex = {'parent': integer, 'key': integer}
         mstPrim[lowestKey] = vertex
-        adjacentNodes = getAllNeighbors(weight, lowestKey) #adjacentNodes = [node, ...] node=integer
+        if mstPrim[lowestKey]['parent'] != None:
+            parent = mstPrim[lowestKey]['parent']
+            if parent < lowestKey:
+                edges[(parent, lowestKey)]['partOfMST'] = True
+            else:
+                edges[(lowestKey, parent)]['partOfMST'] = True
+
+        adjacentNodes = getAllNeighbors(edges, lowestKey) #adjacentNodes = [node, ...] node=integer
         for node in adjacentNodes:
-            if node in graph.keys() and weight[(lowestKey, node)] < graph[node]['key']:
-                graph[node]['parent'] = lowestKey
-                graph[node]['key'] = weight[(lowestKey, node)]
+            if lowestKey < node:
+                if node in graph.keys() and edges[(lowestKey, node)]['weight'] < graph[node]['key']:
+                    graph[node]['parent'] = lowestKey
+                    graph[node]['key'] = edges[(lowestKey, node)]['weight']
+            else:
+                if node in graph.keys() and edges[(node, lowestKey)]['weight'] < graph[node]['key']:
+                    graph[node]['parent'] = lowestKey
+                    graph[node]['key'] = edges[(node, lowestKey)]['weight']
 
     primWeight = 0
     for node in mstPrim:
@@ -36,48 +40,78 @@ def main():
         primWeight += mstPrim[node]['key']
     print "RESULT:"
     print primWeight
+    for edge in edges:
+        tmpEdges = edges.copy()
+        if edges[edge]['partOfMST']:
+            tmpEdges.pop(edge)
+            primLite(noNodes, tmpEdges, edge)
+
+def primLite(noNodes, edges, removedEdge):
+    mstPrim = {}
+    graph = {}
+
+    for i in xrange(0, noNodes):
+        graph[i] = {'key': sys.maxint, 'parent': None}
+    graph[0]['key'] = 0
+
+    while graph:
+        lowestKey = getLowestKey(graph) #lowestKey = integer
+        vertex = graph.pop(lowestKey) #vertex = {'parent': integer, 'key': integer}
+        mstPrim[lowestKey] = vertex
+        adjacentNodes = getAllNeighbors(edges, lowestKey) #adjacentNodes = [node, ...] node=integer
+        for node in adjacentNodes:
+            if lowestKey < node:
+                if node in graph.keys() and edges[(lowestKey, node)]['weight'] < graph[node]['key']:
+                    graph[node]['parent'] = lowestKey
+                    graph[node]['key'] = edges[(lowestKey, node)]['weight']
+            else:
+                if node in graph.keys() and edges[(node, lowestKey)]['weight'] < graph[node]['key']:
+                    graph[node]['parent'] = lowestKey
+                    graph[node]['key'] = edges[(node, lowestKey)]['weight']
+
+    primWeight = 0
+    for node in mstPrim:
+        #print node, mstPrim[node]
+        primWeight += mstPrim[node]['key']
+    '''print "removedEdge: "
+    print removedEdge
+    print "weight:"
+    print primWeight'''
     
 
-def getLowestKey(tempGraph):
+def getLowestKey(graph):
     lowestKey = 0
     lowestKeyWeight = sys.maxint
-    for key in tempGraph.keys():
-        if tempGraph[key]['key'] < lowestKeyWeight:
-            lowestKeyWeight = tempGraph[key]['key']
+    for key in graph.keys():
+        if graph[key]['key'] < lowestKeyWeight:
+            lowestKeyWeight = graph[key]['key']
             lowestKey = key
     return lowestKey
 
-# weight is a dictionary that stores the weight of a path between two vertexes.
-# u is a given vertex
-# getAllNeighbors returns all adjacent vertixes of u
-def getAllNeighbors(weight, u):
+# edges is a dictionary that stores the weight of a path between two vertexes.
+# node is a given vertex
+# getAllNeighbors returns all adjacent vertixes of node
+def getAllNeighbors(edges, node):
     neighbors = []
-    for path in weight:
-        if path[0] == u and path[1] not in neighbors:
+    #print node
+    for path in edges:
+        #print path
+        if path[0] == node:
             neighbors.append(path[1])
+        if path[1] == node:
+            neighbors.append(path[0])
     return neighbors
 
-    '''print weight
-    print weight.keys()
-    keys = weight.keys()
-    #print count
-    i = 0
-    span = []
-    while i < count:
-        lowestWeight = sys.maxint
-        for k in keys:
-            if k[0] == i:
-                if weight[k] < lowestWeight:
-                    lowestWeight = weight[k]
-                    span.append(k)
-        #print i
-        i += 1
-
-    print span
-    shortestPath = 0
-    for i in span:
-        shortestPath += weight[i]
-    print shortestPath'''
-
 if __name__ == '__main__':
-    main()
+    start = time.time()
+    edges = {}
+    noNodes = 0
+    for line in fileinput.input():
+        line = line.split()
+        if not fileinput.isfirstline():
+            edges[(int(line[0]), int(line[1]))] = {'weight': int(line[2]), 'partOfMST': False}
+        else:
+            noNodes = int(line[0])
+    main(noNodes, edges)
+    end = time.time()
+    print end - start
